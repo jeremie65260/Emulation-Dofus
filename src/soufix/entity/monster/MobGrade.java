fa30f27074d2652cbb98639c5aff9ada2720e17f
jeremie65260 <jeremie.lerin@gmail.com>
2025-11-13 00:17:55 +0100

    Nettoyer MobGrade et compiler

--- src/soufix/entity/monster/MobGrade.java
+++ src/soufix/entity/monster/MobGrade.java
@@ -35,7 +35,6 @@ public class MobGrade
   private Map<Integer, Integer> stats=new HashMap<Integer, Integer>();
   private Map<Integer, SortStats> spells=new HashMap<Integer, SortStats>();
   private ArrayList<Integer> statsInfos=new ArrayList<Integer>();
-  private boolean osamodasScalingApplied;
 
   public MobGrade(Monster template, int grade, int level, int pa, int pm, String resists, String stats, String statsInfos, String allSpells, int pdvMax, int aInit, int xp, int n)
   {
@@ -331,22 +330,35 @@ public class MobGrade
         // Prendre les stats de l'invocateur
         if(caster.getPersonnage().getClasse()==Constant.CLASS_OSAMODAS)
         {
-          final double transferRatio=0.8d;
+          final double vitalityRatio=0.25d;
+          final double characteristicsRatio=0.5d;
+          final double damagesRatio=0.4d;
           Stats invocatorStats=caster.getPersonnage().getTotalStats();
 
-          int vitalityBonus=(int)Math.floor(invocatorStats.getEffect(Constant.STATS_ADD_VITA)*transferRatio);
+          int vitalityBonus=(int)Math.floor(invocatorStats.getEffect(Constant.STATS_ADD_VITA)*vitalityRatio);
           if(vitalityBonus>0)
           {
             pdv=pdvMax+vitalityBonus;
             pdvMax=pdv;
           }
 
-          int[] statsToTransfer={ Constant.STATS_ADD_SAGE, Constant.STATS_ADD_FORC, Constant.STATS_ADD_INTE,
-              Constant.STATS_ADD_CHAN, Constant.STATS_ADD_AGIL, Constant.STATS_ADD_DOMA, Constant.STATS_ADD_PERDOM,
-              Constant.STATS_ADD_PDOM };
-          for(int statId : statsToTransfer)
+          int[] characteristicStats={ Constant.STATS_ADD_SAGE, Constant.STATS_ADD_FORC, Constant.STATS_ADD_INTE,
+              Constant.STATS_ADD_CHAN, Constant.STATS_ADD_AGIL };
+          for(int statId : characteristicStats)
           {
-            int addition=(int)Math.floor(invocatorStats.getEffect(statId)*transferRatio);
+            int addition=(int)Math.floor(invocatorStats.getEffect(statId)*characteristicsRatio);
+            if(addition<=0)
+              continue;
+            Integer baseValue=this.stats.get(statId);
+            if(baseValue==null)
+              baseValue=0;
+            this.stats.put(statId,baseValue+addition);
+          }
+
+          int[] damageStats={ Constant.STATS_ADD_DOMA, Constant.STATS_ADD_PERDOM, Constant.STATS_ADD_PDOM };
+          for(int statId : damageStats)
+          {
+            int addition=(int)Math.floor(invocatorStats.getEffect(statId)*damagesRatio);
             if(addition<=0)
               continue;
             Integer baseValue=this.stats.get(statId);
