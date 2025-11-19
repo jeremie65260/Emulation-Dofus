5c05cfe80a5b9550aaabb77bea1d78dfc36a8616
jeremie65260 <jeremie.lerin@gmail.com>
2025-11-19 20:45:50 +0100

    Revert "Allow job interactions without tools except fishing"

--- src/soufix/job/Job.java
+++ src/soufix/job/Job.java
@@ -8,7 +8,6 @@ import java.util.Map.Entry;
 import soufix.client.Player;
 import soufix.job.fm.Rune;
 import soufix.object.GameObject;
-import soufix.job.JobConstant;
 
 public class Job
 {
@@ -82,9 +81,6 @@ public class Job
 
   public boolean isValidTool(int id1)
   {
-    if(this.id!=JobConstant.JOB_PECHEUR)
-      return true;
-
     if(id1==20053 && this.id!=36)
       return true;
 
