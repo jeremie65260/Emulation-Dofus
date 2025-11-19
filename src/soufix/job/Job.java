09d7d27dcb15fdf0a46484514cf5992f2c4ff948
jeremie65260 <jeremie.lerin@gmail.com>
2025-11-19 21:04:43 +0100

    Allow universal tool 20053 for non-fisher jobs

--- src/soufix/job/Job.java
+++ src/soufix/job/Job.java
@@ -8,6 +8,7 @@ import java.util.Map.Entry;
 import soufix.client.Player;
 import soufix.job.fm.Rune;
 import soufix.object.GameObject;
+import soufix.job.JobConstant;
 
 public class Job
 {
@@ -81,7 +82,10 @@ public class Job
 
   public boolean isValidTool(int id1)
   {
-    if(id1==20053 && this.id!=36)
+    if(id1==20053 && this.id!=JobConstant.JOB_PECHEUR)
+      return true;
+
+    if(this.id!=JobConstant.JOB_PECHEUR)
       return true;
 
     for(int id : this.tools)
