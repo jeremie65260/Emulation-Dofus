1fd295d79a4d52cdfb3c1ce939fcc4c7b562b9a0
jeremie65260 <jeremie.lerin@gmail.com>
2025-12-22 22:21:41 +0100

    Update arena reward victories to map 15072

--- src/soufix/fight/Fight.java
+++ src/soufix/fight/Fight.java
@@ -5524,11 +5524,14 @@ public void Anti_bug () {
       loosers.addAll(this.getTeam0().values());
     }
 
-    if(this.getMapOld()!=null&&this.getMapOld().getId()==15080&&win==1)
+    if(this.getMapOld()!=null&&this.getMapOld().getId()==15072&&win==1)
     {
       for(Fighter fighter : winners)
         if(fighter!=null&&fighter.getPersonnage()!=null)
+        {
           fighter.getPersonnage().clearGladiatroolCheckpoint();
+          fighter.getPersonnage().applyArenaRewardVictoryBonus();
+        }
     }
 
     if(this.getType()==Constant.FIGHT_TYPE_PVM&&this.getMapOld()!=null&&Constant.isGladiatroolMap(this.getMapOld().getId()))
